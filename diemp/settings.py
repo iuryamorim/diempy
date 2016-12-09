cced1acd51383ee5fb4b734f294dabb342c26c29
iury amorim <iuryamorimrj@gmail.com>
2016-12-09 14:20:37 -0200

    Ajustes para apache

--- diemp/settings.py
+++ diemp/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 #Email configuration
 EMAIL_BACKEND = config('EMAIL_BACKEND')
