95f024939356db4bcba61b5e703b0a89cc666caa
iury amorim <iuryamorimrj@gmail.com>
2017-01-06 20:16:50 -0200

    Ajustes para nova planilha de importação

--- diemp/aluno/views.py
+++ diemp/aluno/views.py
@@ -27,6 +27,7 @@ def create(request):
         if not form.is_valid():
             return render(request, 'aluno/import_csv.html', {'form': form})
 
+        
         try:
             handle_files(request)
         except:
@@ -37,69 +38,45 @@ def create(request):
 
 def handle_files(request):
     f = TextIOWrapper(request.FILES['file'].file, encoding=request.encoding)
-    reader = csv.reader(f)
-    cont = 0
+    reader = csv.DictReader(f)
     for row in reader:
-        if cont != 0 :
-            NOME = row[0]
-            MATRICULA = row[1]
-            COD_CURSO = row[2]
-            CPF = row[4]
-            SITUACAO = row[6]
-            DDI_RESIDENCIAL = row[7]
-            DDD_RESIDENCIAL = row[8]
-            FONE_RESIDENCIAL = row[9]
-            DDI_COMERCIAL = row[10]
-            DDD_COMERCIAL = row[11]
-            FONE_COMERCIAL = row[12]
-            DDI_CELULAR = row[13]
-            DDD_CELULAR = row[14]
-            FONE_CELULAR = row[15]
-            TIPO_LOGRADOURO = row[16]
-            LOGRADOURO = row[17]
-            NUMERO = row[18]
-            COMPLEMENTO = row[19]
-            BAIRRO = row[20]
-            CEP = row[21]
-            DISTRITO = row[22]
-            MUNICIPIO = row[23]
-            UF = row[24]
-            PAIS = row[25]
-            EMAIL = row[26]
-            DT_NASC = row[27].split('/')
-            DT_NASC = (DT_NASC[2] + '-' + DT_NASC[1] + '-' + DT_NASC[0])
+                
+        for r in row.keys():
+            if row[r] == 'NULL':
+                row[r] = None                    
 
-            obj = Inscricao(nome=NOME, email=EMAIL, cpf=CPF, dt_nasc=DT_NASC, situacao=SITUACAO,
-                            ddi_residencial=DDI_RESIDENCIAL, ddd_residencial=DDD_RESIDENCIAL,
-                            fone_residencial=FONE_RESIDENCIAL,
-                            ddi_comercial=DDI_COMERCIAL, ddd_comercial=DDD_COMERCIAL, fone_comercial=FONE_COMERCIAL,
-                            ddi_celular=DDI_CELULAR, ddd_celular=DDD_CELULAR, fone_celular=FONE_CELULAR,
-                            tipo_logradouro=TIPO_LOGRADOURO,
-                            logradouro=LOGRADOURO, numero=NUMERO, complemento=COMPLEMENTO, bairro=BAIRRO, cep=CEP,
-                            distrito=DISTRITO,
-                            municipio=MUNICIPIO, uf=UF, pais=PAIS)
+        obj = Inscricao(nome=row['NOME_ALUNO'], email=row['E_MAIL'], cpf=row['CPF'], dt_nasc=row['DT_NASCIMENTO'], situacao=row['SITUACAO'],
+                        ddi_residencial=row['DDI_RESIDENCIAL'], ddd_residencial=row['DDD_RESIDENCIAL'],
+                        fone_residencial=row['FONE_RESIDENCIAL'],
+                        ddi_comercial=row['DDI_COMERCIAL'], ddd_comercial=row['DDD_COMERCIAL'], fone_comercial=row['FONE_COMERCIAL'],
+                        ddi_celular=row['DDI_CELULAR'], ddd_celular=row['DDD_CELULAR'], fone_celular=row['FONE_CELULAR'],
+                        tipo_logradouro=row['TIPO_LOGRADOURO'],
+                        logradouro=row['LOGRADOURO'], numero=row['NUMERO'], complemento=row['COMPLEMENTO'], bairro=row['BAIRRO'], cep=row['CEP'],
+                        distrito=row['DISTRITO'],
+                        municipio=row['MUNICIPIO'], uf=row['UF'], pais=row['PAIS'])
 
-            try:
-                objAluno = Inscricao.objects.get(cpf=CPF)
-                id = objAluno.pk
-            except:
-                id = None
+        try:
+            objAluno = Inscricao.objects.get(cpf=row['CPF'])
+            id = objAluno.pk
+        except:
+            id = None
 
-            if id:
-                obj.id = id
-            obj.save()
-
-            obj_curso = Curso.objects.get(cod_curso=COD_CURSO)
-            try:
-                objpessoacurso = PessoaCurso.objects.get(id_pessoa=obj.pk)
-            except:
-                objpessoacurso = None
-            if objpessoacurso:
-                if (objpessoacurso.id_curso == obj_curso.pk):
-                    continue
-            
-            obj_ps = PessoaCurso(nome_curso=obj_curso, nome_aluno=obj, matricula=MATRICULA)
-            obj_ps.save()
-        else:
-            cont += 1
+        if id:
+            obj.id = id
+        obj.save()
+        
+        try:
+            obj_curso = Curso.objects.get(cod_curso=row['COD_CURSO'])
+        except:
+            obj_curso = Curso.objects.create(cod_curso=row['COD_CURSO'], unidade=row['UNIDADE'], nome=row['CURSO'])
+        try:
+            objpessoacurso = PessoaCurso.objects.get(id_pessoa=obj.pk)
+        except:
+            objpessoacurso = None
+        if objpessoacurso:
+            if (objpessoacurso.id_curso == obj_curso.pk):
+                continue
+        
+        obj_ps = PessoaCurso(nome_curso=obj_curso, nome_aluno=obj, matricula=row['MATRICULA'])
+        obj_ps.save()
     return True
