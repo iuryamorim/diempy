22ed616c8c6be5ad2e55abd8a4df9b8a94c9dcfa
iury amorim <iuryamorimrj@gmail.com>
2017-03-27 21:43:08 -0300

    correções para a importação de alunos

--- diemp/aluno/views.py
+++ diemp/aluno/views.py
@@ -26,7 +26,6 @@ def create(request):
         form = ImportForm(request.POST, request.FILES)
         if not form.is_valid():
             return render(request, 'aluno/import_csv.html', {'form': form})
-
         try:
             handle_files(request)
         except:
@@ -42,32 +41,47 @@ def handle_files(request):
        
         for r in row.keys():
             if row[r] == 'NULL':
-                row[r] = None                    
+                row[r] = None
 
-        obj = Inscricao(nome=row['NOME_ALUNO'], email=row['E_MAIL'], cpf=row['CPF'], dt_nasc=row['DT_NASCIMENTO'], situacao=row['SITUACAO'],
-                        ddi_residencial=row['DDI_RESIDENCIAL'], ddd_residencial=row['DDD_RESIDENCIAL'],
-                        fone_residencial=row['FONE_RESIDENCIAL'],
-                        ddi_comercial=row['DDI_COMERCIAL'], ddd_comercial=row['DDD_COMERCIAL'], fone_comercial=row['FONE_COMERCIAL'],
-                        ddi_celular=row['DDI_CELULAR'], ddd_celular=row['DDD_CELULAR'], fone_celular=row['FONE_CELULAR'],
-                        tipo_logradouro=row['TIPO_LOGRADOURO'],
-                        logradouro=row['LOGRADOURO'], numero=row['NUMERO'], complemento=row['COMPLEMENTO'], bairro=row['BAIRRO'], cep=row['CEP'],
-                        distrito=row['DISTRITO'],
-                        municipio=row['MUNICIPIO'], uf=row['UF'], pais=row['PAIS'])
+        DTNASC = is_none(row['DT_NASCIMENTO'])
+        if is_none(row['DT_NASCIMENTO']):
+            try:
+                DTNASC = DTNASC.split("/")
+                DTNASC = DTNASC[2] + "-" + DTNASC[1] + "-" + DTNASC[0]
+            except Exception as e:
+                DTNASC = None
+                print("Erro ao parsear a Data de Nascimento do cpf: " + is_none(row['CPF']))
+                print(e)
+
+        obj = Inscricao(nome=is_none(row['NOME_ALUNO']), email=is_none(row['E_MAIL']), cpf=is_none(row['CPF']), dt_nasc=DTNASC, situacao=is_none(row['SITUACAO']),
+                        ddi_residencial=is_none(row['DDI_RESIDENCIAL']), ddd_residencial=is_none(row['DDD_RESIDENCIAL']),
+                        fone_residencial=is_none(row['FONE_RESIDENCIAL']),
+                        ddi_comercial=is_none(row['DDI_COMERCIAL']), ddd_comercial=is_none(row['DDD_COMERCIAL']), fone_comercial=is_none(row['FONE_COMERCIAL']),
+                        ddi_celular=is_none(row['DDI_CELULAR']), ddd_celular=is_none(row['DDD_CELULAR']), fone_celular=is_none(row['FONE_CELULAR']),
+                        tipo_logradouro=is_none(row['TIPO_LOGRADOURO']),
+                        logradouro=is_none(row['LOGRADOURO']), numero=is_none(row['NUMERO']), complemento=is_none(row['COMPLEMENTO']), bairro=is_none(row['BAIRRO']), cep=is_none(row['CEP']),
+                        distrito=is_none(row['DISTRITO']),
+                        municipio=is_none(row['MUNICIPIO']), uf=is_none(row['UF']), pais=is_none(row['PAIS']))
 
         try:
-            objAluno = Inscricao.objects.get(cpf=row['CPF'])
+            objAluno = Inscricao.objects.get(cpf=is_none(row['CPF']))
             id = objAluno.pk
         except:
             id = None
 
         if id:
             obj.id = id
-        obj.save()
-        
         try:
-            obj_curso = Curso.objects.get(cod_curso=row['COD_CURSO'])
+            obj.save()
+        except Exception as err:
+            print(err)
+            continue
+
+        try:
+            obj_curso = Curso.objects.get(cod_curso=is_none(row['COD_CURSO']))
         except:
-            obj_curso = Curso.objects.create(cod_curso=row['COD_CURSO'], unidade=row['UNIDADE'], nome=row['CURSO'])
+            obj_curso = Curso.objects.create(cod_curso=is_none(row['COD_CURSO']),
+                                unidade=is_none(row['UNIDADE']), nome=is_none(row['CURSO']))
         try:
             objpessoacurso = PessoaCurso.objects.get(id_pessoa=obj.pk)
         except:
@@ -76,6 +90,12 @@ def handle_files(request):
             if (objpessoacurso.id_curso == obj_curso.pk):
                 continue
         
-        obj_ps = PessoaCurso(nome_curso=obj_curso, nome_aluno=obj, matricula=row['MATRICULA'])
+        obj_ps = PessoaCurso(nome_curso=obj_curso, nome_aluno=obj, matricula=is_none(row['MATRICULA']))
         obj_ps.save()
     return True
+
+
+def is_none(value):
+    if len(value) == 0:
+        return None
+    return value
